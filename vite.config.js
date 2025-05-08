05d70a40893560f005c7198ff2eddc355537d7c4
madfozjer <fozerion00@proton.me>
2025-05-08 16:39:08 +0200

    public path fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: process.env.NODE_ENV === "production" ? "/goldenballrace/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
