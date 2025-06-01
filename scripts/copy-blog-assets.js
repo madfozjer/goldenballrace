4c2d3c0ffc5f8c49a853c22ff116610a0a172caa
madfozjer <fozerion00@proton.me>
2025-06-01 13:16:10 +0200

    __dirname shit fixed

--- scripts/copy-blog-assets.js
+++ scripts/copy-blog-assets.js
@@ -1,9 +1,13 @@
 import fs from "fs-extra";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const mainAppDistDir = path.resolve(__dirname, "../dist");
 const vuepressTempDistDir = path.resolve(__dirname, "../docs/dist-blog-temp");
-const blogTargetDir = path.join(mainAppDistDir, "blog");
+const blogTargetDir = path.join(mainAppDistDir, "blog"); // Where blog will live in final dist
 
 console.log("--- Copying VuePress blog assets ---");
 console.log(`Source: ${vuepressTempDistDir}`);
